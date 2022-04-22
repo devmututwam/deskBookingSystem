89bef1ba656c9abacd1d921df5a32db78f50292a
opaydevzambia <opaydevzambia@gmail.com>
2022-04-22 12:09:23 +0200

    Save work

---
+++ src/test/java/com/deskbookingsystem/DeskBookingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deskbookingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeskBookingSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
